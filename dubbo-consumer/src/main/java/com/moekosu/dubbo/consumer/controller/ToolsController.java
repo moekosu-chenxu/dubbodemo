5bf2523f884a0d613c0c4ce2a4253f2a1a4c6a94
moekosu <120921604@qq.com>
2018-02-26 15:56:01 +0800

    download io

--- dubbo-consumer/src/main/java/com/moekosu/dubbo/consumer/controller/ToolsController.java
+++ dubbo-consumer/src/main/java/com/moekosu/dubbo/consumer/controller/ToolsController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -33,22 +38,53 @@ public class ToolsController {
 
     @RequestMapping("/tools/download")
     @ResponseBody
-    public String downloadTools(String id)
+    public String downloadTools(String downloadUrl, HttpServletResponse response)
     {
-        Integer toolsID = 0;
-        try {
-            toolsID = Integer.valueOf(id);
-        }
-        catch (Exception e)
-        {
-            // logging
-        }
-        Tools tools = new Tools();
-        tools.setId(toolsID);
-        List<Tools> list = toolsService.getToolsList(tools);
-        Tools result = !list.isEmpty() && list.size()>0? list.get(0) : new Tools();
+        //
+        final String msgSuccess = "成功";
+        final String msgNoExist = "失败，文件不存在";
+        final String msgNoAuth = "失败，您无权限下载";
+        final String msgError = "失败，系统错误";
 
-        return JSON.toJSONString(result);
+        // 处理
+        File f = new File(downloadUrl);
+        if(!f.exists()){
+            return msgNoExist;
+        }
+        // TODO 鉴权(提取密码)
+
+        //
+        FileInputStream in = null;
+        ServletOutputStream out = null;
+        try {
+            // 缓存区1024个字节
+            byte[] buf = new byte[1024];
+            int rc = 0;
+            // 文件输入流
+            in = new FileInputStream(f);
+            // response的输出流
+            out = response.getOutputStream();
+            // 写入out(每次读取指定buf长度的字符，指针已走到最后位置)
+            while((rc = in.read(buf, 0, buf.length)) > 0){
+                out.write(buf, 0, rc);
+            }
+            out.flush();
+        }
+        catch ( Exception e )
+        {
+            return msgError;
+        }
+        finally {
+            try {
+                in.close();
+                out.close();
+            }
+            catch ( IOException e ){
+
+            }
+        }
+
+        return msgSuccess;
     }
 
 }
